e5cb33205b11470f72a422859b89a53cad75d12b
oleg086rus <o.pototskiy@yandex.ru>
2022-06-23 01:38:45 +0500

    dispatch

--- src/index.js
+++ src/index.js
@@ -6,13 +6,24 @@ function createStore(initialState) {
   function getState() {
     return state
   }
-  return {getState}
+  function dispatch(action) {
+    console.log(action)
+    if (action.type === "task/completed") {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el=>el.id===action.payload.id)
+      newArray[elementIndex].completed = true
+      state = newArray
+      console.log(state)
+    }
+  }
+  return {getState, dispatch}
 }
 const store = createStore([{id:1, description: 'Task 1', completed: false}])
 
 const App = (params) => {
   console.log(store.getState())
-  return <h1>Фзз</h1>
+  // store.dispatch({type: 'task/completed', preload: {id:1}})
+  return <><h1>Фзз</h1><button onClick={()=>store.dispatch({type: "task/completed", payload: {id:1}})}>Completed</button></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
