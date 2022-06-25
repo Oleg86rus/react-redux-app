c71c17431fb89ce75d9faafb4ecfcdec3959c5b4
oleg086rus <o.pototskiy@yandex.ru>
2022-06-25 15:36:21 +0500

    react-redux providing store

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
   titleChanged,
 } from './store/task'
 import configureStore from './store/store'
+import { Provider } from 'react-redux'
 
 const store = configureStore()
 
@@ -47,6 +48,8 @@ const App = (params) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
